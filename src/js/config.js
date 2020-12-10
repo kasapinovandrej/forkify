e242ee74ba2487e23486b337d2e6e254e7e85b53
kasapinovandrej <kasapinovandrej@gmail.com>
2020-12-10 18:39:25 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'acc70afb-6603-4957-8173-f286bd574074'
+export const MODAL_CLOSE_SEC = 2.5
